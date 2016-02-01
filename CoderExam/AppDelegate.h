63bc56d51cb55209205d4eb3dc2ef15be81bae66
刘少明 <liushaoming@liushaomingdemacbook-air.local>
2016-02-01 21:22:24 +0800

    Initial Commit

---
+++ CoderExam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoderExam
+//
+//  Created by 刘少明 on 16/2/1.
+//  Copyright © 2016年 FJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoderExam/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoderExam
+//
+//  Created by 刘少明 on 16/2/1.
+//  Copyright © 2016年 FJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
